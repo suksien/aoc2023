e225f49ce0d808c9f27c44552193b08f79732f42
suksien <hahasuksien@yahoo.com.my>
2023-12-16 12:06:33 +0800

    day 13

--- day13.rb
+++ day13.rb
@@ -141,54 +141,81 @@ def part1(filename)
   p (ncol + nrow * 100)
 end
 
-#part1("test_input_day13.txt") # 405
-#part1("input_day13.txt") # 29165
+part1("test_input_day13.txt") # 405
+part1("input_day13.txt") # 29165
 
 =begin
 ALGO:
+
+one line mismatch
+one spot mismatch
 1. Find smudge
 2. Fix smudge
 3. Find potential mirrors
+
+brute force: find potential mirrors after inverting every char of pattern
 =end
 
 def reshape(arr, ncol)
   arr.each_slice(ncol).to_a
 end
 
-def invert_one_char(pattern, index, ncol)
-  one_line = pattern.join.chars
-  char = one_line[index]
-  one_line[index] = (char == '.' ? '#' : '.')
-  reshape(one_line, ncol)
+def invert_one_char(pattern, irow_char, icol_char)
+  pattern = pattern.map do |row|
+    row.map do |char|
+      char.dup
+    end
+  end
+
+  char = pattern[irow_char][icol_char]
+  pattern[irow_char][icol_char] = (char == '.' ? '#' : '.')
+  pattern
+end
+
+def find_horizontal_mirror_smudge(pattern, irow_smudge)
+  mirror = {}
+  pattern = pattern.map(&:join)
+
+  (1..pattern.size-1).each do |index|
+    top_half = pattern[0..index-1]
+    bottom_half = pattern[index..-1]
+
+    n_elem = [top_half.size, bottom_half.size].min
+    top_half.reverse!
+    if (0..n_elem-1).all? { |i| top_half[i] == bottom_half[i] }
+      first_reflected_row_index = index - n_elem
+      last_reflected_row_index = index + n_elem - 1
+      #p "range", [first_reflected_row_index, last_reflected_row_index, irow_smudge]
+      mirror[index] = n_elem if (first_reflected_row_index..last_reflected_row_index).include?(irow_smudge)
+    end 
+  end
+  mirror
 end
 
 def part2(filename)
   patterns =  parse(filename)
   ncol, nrow = 0, 0
 
-  patterns = [patterns[1]]
-
   patterns.each do |pattern|
-    p pattern[0].join
-    nrow_pattern, ncol_pattern = pattern.size, pattern[0].size
     all_mirrors = []
+    nrow_pattern, ncol_pattern = pattern.size, pattern[0].size
 
-    #(nrow_pattern * ncol_pattern).times do |i|
-    5.times do|i|
-      new_pattern = invert_one_char(pattern, i, ncol_pattern)
-      row_mirrors = find_horizontal_mirror(new_pattern)
-      col_mirrors = find_horizontal_mirror(new_pattern.transpose)
-      # have to be able to "see" the smudge from the mirror
-      direction, mirror_index = pick_perfect_mirror(row_mirrors, col_mirrors)
-      all_mirrors << [direction, mirror_index] unless mirror_index.nil? || all_mirrors.include?([direction, mirror_index])
+    nrow_pattern.times do |irow_smudge|
+      ncol_pattern.times do |icol_smudge|
+        new_pattern = invert_one_char(pattern, irow_smudge, icol_smudge)
+        row_mirrors = find_horizontal_mirror_smudge(new_pattern, irow_smudge)
+        col_mirrors = find_horizontal_mirror_smudge(new_pattern.transpose, icol_smudge)
+        direction, mirror_index = pick_perfect_mirror(row_mirrors, col_mirrors)
+        all_mirrors << [direction, mirror_index] unless mirror_index.nil? || all_mirrors.include?([direction, mirror_index])
+      end
     end
-
-    #p all_mirrors
-
-    # ncol += mirror_index if direction == "col"
-    # nrow += mirror_index if direction == "row"
+    all_mirrors = all_mirrors.flatten
+    direction, mirror_index = all_mirrors[0], all_mirrors[1]
+    ncol += mirror_index if direction == "col"
+    nrow += mirror_index if direction == "row"
   end
-  #p (ncol + nrow * 100)
+  p (ncol + nrow * 100)
 end
 
-part2("test_input_day13.txt")
+part2("test_input_day13.txt") # 400
+part2("input_day13.txt") # 32192
